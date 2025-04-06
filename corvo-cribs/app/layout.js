4b1b55bbe2d1f9fb8976ba03b5003f2488077012
Luca Denhez <lucadenhez@icloud.com>
2025-04-05 17:55:08 -0700

    Updates

--- corvo-cribs/app/layout.js
+++ corvo-cribs/app/layout.js
@@ -1,6 +1,8 @@
 import { Jua } from "next/font/google";
 import "./globals.css";
+
 import Navbar from "./componenets/Navbar";
+import Onboarding_1 from "./onboarding_1/page";
 
 const jua = Jua({
   weight: "400",
@@ -12,11 +14,24 @@ export const metadata = {
   description: "Web app for Corvo Cribs.",
 };
 
+const isOnboarded = false;
+
 export default function RootLayout({ children }) {
+
+  if (!isOnboarded) {
+    return (
+      <html lang="en">
+        <body className={`${jua.className} antialiased m-3`}>
+          <Onboarding_1 />
+        </body>
+      </html>
+    );
+  }
+
   return (
     <html lang="en">
       <body
-        className={ `${jua.className} antialiased m-3` }
+        className={`${jua.className} antialiased m-3`}
       >
         <Navbar />
         {children}
--- corvo-cribs/app/onboarding_1/page.js
+++ corvo-cribs/app/onboarding_1/page.js
@@ -1,11 +1,14 @@
 import Image from "next/image";
 
 export default function Onboarding_1() {
-  return (
-    <div className="flex flex-col items-center justify-between h-full ">
-        <h1 className="text-3xl font-semibold text-center m-12">Looking for housing you next academic term?</h1>
-        <button className="bg-[#69554D] hover:bg-[#5B4B44] text-white font-bold py-2 px-4 rounded mt-auto">Yes!</button>
-    </div>
-    
-  );
+    return (
+        <div className="h-screen">
+            <div className="pt-15 flex flex-col items-center justify-between h-full gap-y-5  ">
+                <h1 className="text-3xl font-semibold text-center">Looking for housing you next academic term?</h1>
+                <div className="pb-15">
+                    <button className="bg-[#69554D] hover:bg-[#5B4B44] text-white font-bold py-2 px-4 rounded mt-auto">Yes!</button>
+                </div>
+            </div>
+        </div>
+    );
 }
