747ef89782b1049cd9dfacb992d13a0dc77104e7
0Tristan0 <goehrint@oregonstate.edu>
2025-04-05 21:54:24 -0700

    finished prefrences page

--- corvo-cribs/app/onboarding_3/page.js
+++ corvo-cribs/app/onboarding_3/page.js
@@ -1,17 +1,19 @@
 import Image from "next/image";
 
 export default function Onboarding_1() {
-    return (
-        <div className="h-screen">
-            <div className="pt-15 flex flex-col items-center justify-between h-full gap-y-5  ">
-                <h1 className="text-7xl font-semibold text-center">Corvo Cribs</h1>
-                <div className="pb-20">
-                  <img className=""></img>
-                </div>
-                <div className="pb-15">
-                    <button className="bg-[#69554D] hover:bg-[#29201D] text-white text-2xl  py-2 px-4 rounded w-60 h-15">Lets get started!</button>
-                </div>
-            </div>
+  return (
+    <div className="h-screen">
+      <div className="pt-15 flex flex-col items-center justify-between h-full gap-y-5  ">
+        <h1 className="text-7xl font-semibold text-center">Corvo Cribs</h1>
+        <div className="pb-20">
+          <img className=""></img>
         </div>
-    );
+        <div className="pb-15">
+          <button className="bg-[#69554D] hover:bg-[#29201D] text-white text-2xl  py-2 px-4 rounded w-60 h-15">
+            Lets get started!
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
--- corvo-cribs/app/onboarding_preferences/page.js
+++ corvo-cribs/app/onboarding_preferences/page.js
@@ -1,16 +1,194 @@
+"use client";
 import Image from "next/image";
-
+import Link from "next/link";
+import { useState } from "react";
 export default function Onboarding_1() {
-    return (
-        <div className="h-screen">
-            <div className="pt-15 flex flex-col items-center justify-between h-full gap-y-5  ">
-                <div className="pb-15">
-                    <button className="bg-[#69554D] hover:bg-[#29201D] text-white text-2xl  py-2 px-4 rounded w-60 h-15">Furnished</button>
-                </div>
-                <div className="pb-125">
-                    <button className="bg-[#69554D] hover:bg-[#29201D] text-white text-2xl  py-2 px-4 rounded w-60 h-15">Bathrooms</button>
-                </div>
-            </div>
-        </div>
-    );
+  const [isFurnished, setIsFurnished] = useState(true);
+  const [bedrooms, setBedrooms] = useState(1);
+  const [bathrooms, setBathrooms] = useState(1);
+  const [price, setPrice] = useState(500);
+  const [term, setTerm] = useState(1);
+  const [walk, setWalk] = useState(0);
+  const [bike, setBike] = useState(0);
+  const [publicTransportation, setpublicTransportation] = useState(0);
+
+  const handleBedroomsChange = (event) => {
+    setBedrooms(event.target.value);
+    if (bedrooms < 0) {
+      setBedrooms = 0;
+    } else if (bedrooms > 10) {
+      setBedrooms = 10;
+    }
+  };
+
+  const handlePriceChange = (event) => {
+    setPrice(event.target.value);
+  };
+  const handleLeaseTermChange = (event) => {
+    setTerm(event.target.value);
+  };
+
+  const handleWalkChange = (event) => {
+    setWalk(event.target.value);
+  };
+
+  const handleBikeChange = (event) => {
+    setBike(event.target.value);
+  };
+
+  const handlePublicTransportationChange = (event) => {
+    setpublicTransportation(event.target.value);
+  };
+
+  const formatTime = (minutes) => {
+    const hours = Math.floor(minutes / 60);
+    const remainingMinutes = minutes % 60;
+    if (hours > 0) {
+      return `${hours} hr${hours > 1 ? "s" : ""} ${remainingMinutes} min${
+        remainingMinutes !== 1 ? "s" : ""
+      }`;
+    }
+    return `${remainingMinutes} min${remainingMinutes !== 1 ? "s" : ""}`;
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-between h-full">
+      <button
+        onClick={() => setIsFurnished((prevState) => !prevState)}
+        className={`text-white font-bold text-2xl py-2 px-4 rounded w-64 h-16 mb-8 ${
+          isFurnished ? "bg-[#69554D]" : "bg-[#AB8C80]"
+        }`}
+      >
+        Furnished
+      </button>
+      <div
+        className={
+          "text-white font-bold bg-[#69554D] text-2xl py-2 px-4 rounded w-64 h-16 mb-8 flex justify-between items-center"
+        }
+      >
+        <button
+          onClick={() => setBedrooms(bedrooms + 1)}
+          className="flex items-center "
+        >
+          <Image src="/icons/+.svg" width={24} height={24}></Image>
+        </button>
+        Bedrooms: {bedrooms}{" "}
+        <button
+          onClick={() => setBedrooms(bedrooms - 1)}
+          className="flex items-center "
+        >
+          <Image src="/icons/Minus.svg" width={24} height={24}></Image>
+        </button>
+      </div>
+
+      <div
+        className={
+          "text-white font-bold bg-[#69554D] text-2xl py-2 px-4 rounded w-64 h-16 mb-8 flex justify-between items-center"
+        }
+      >
+        <button
+          onClick={() => setBathrooms(bathrooms + 1)}
+          className="flex items-center "
+        >
+          <Image src="/icons/+.svg" width={24} height={24}></Image>
+        </button>
+        Bathrooms: {bathrooms}{" "}
+        <button
+          onClick={() => setBathrooms(bathrooms - 1)}
+          className="flex items-center "
+        >
+          <Image src="/icons/Minus.svg" width={24} height={24}></Image>
+        </button>
+      </div>
+
+      <div className="flex flex-col items-center w-64 mb-8">
+        <label htmlFor="price" className="text-xl font-semibold mb-2">
+          {" "}
+          Price: ${price} mo.
+        </label>
+        <input
+          type="range"
+          id="price"
+          min="500"
+          max="5000"
+          step="100"
+          value={price}
+          onChange={handlePriceChange}
+          className="w-full accent-[#69554D] h-2 bg-gray-200 rounded-lg"
+        />
+      </div>
+
+      <div className="flex flex-col items-center w-64 mb-8">
+        <label htmlFor="lease" className="text-xl font-semibold mb-2">
+          Lease term: {term} mo.
+        </label>
+        <input
+          type="range"
+          id="lease"
+          min="1"
+          max="12"
+          step="1"
+          value={term}
+          onChange={handleLeaseTermChange}
+          className="w-full accent-[#69554D] h-2 bg-gray-200 rounded-lg"
+        />
+      </div>
+
+      <div className="flex flex-col items-center w-64 mb-8">
+        <label htmlFor="walk" className="text-.5xl font-semibold mb-2">
+          Walking distance: {formatTime(walk)}
+        </label>
+        <input
+          type="range"
+          id="walk"
+          min="0"
+          max="120"
+          step="5"
+          value={walk}
+          onChange={handleWalkChange}
+          className="w-full accent-[#69554D] h-2 bg-gray-200 rounded-lg"
+        />
+      </div>
+
+      <div className="flex flex-col items-center w-64 mb-8">
+        <label htmlFor="bike" className="text-.5xl font-semibold mb-2">
+          Biking distance: {formatTime(bike)}
+        </label>
+        <input
+          type="range"
+          id="bike"
+          min="0"
+          max="120"
+          step="5"
+          value={bike}
+          onChange={handleBikeChange}
+          className="w-full accent-[#69554D] h-2 bg-gray-200 rounded-lg"
+        />
+      </div>
+
+      <div className="flex flex-col items-center w-64 mb-8">
+        <label htmlFor="public" className="text-.5xl font-semibold mb-2">
+          Public trasportation: {formatTime(publicTransportation)}
+        </label>
+        <input
+          type="range"
+          id="public"
+          min="0"
+          max="120"
+          step="5"
+          value={publicTransportation}
+          onChange={handlePublicTransportationChange}
+          className="w-full accent-[#69554D] h-2 bg-gray-200 rounded-lg"
+        />
+      </div>
+
+      <div>
+        <Link href="\">
+          <button>
+            <Image src="/icons/Arrow.svg" width={60} height={40}></Image>
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 }
