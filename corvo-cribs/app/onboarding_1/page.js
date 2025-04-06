ddcfd9e8134265a5b96784fbc2331424ef210abb
0Tristan0 <goehrint@oregonstate.edu>
2025-04-05 17:45:20 -0700

    Onboarding_1 page

--- corvo-cribs/app/onboarding_1/page.js
+++ corvo-cribs/app/onboarding_1/page.js
@@ -2,8 +2,10 @@ import Image from "next/image";
 
 export default function Onboarding_1() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1 className="text-3xl font-semibold text-center">Onboarding 1</h1>
+    <div className="flex flex-col items-center justify-between h-full ">
+        <h1 className="text-3xl font-semibold text-center m-12">Looking for housing you next academic term?</h1>
+        <button className="bg-[#69554D] hover:bg-[#5B4B44] text-white font-bold py-2 px-4 rounded mt-auto">Yes!</button>
     </div>
+    
   );
 }
