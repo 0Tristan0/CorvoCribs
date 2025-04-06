70339b93bd66b42fb4fa31d0e040211ac6beb7de
Luca Denhez <lucadenhez@icloud.com>
2025-04-06 02:06:01 -0700

    Fixed an onboarding page

--- corvo-cribs/app/onboarding_3/page.js
+++ corvo-cribs/app/onboarding_3/page.js
@@ -5,25 +5,18 @@ export default function Onboarding_3() {
   return (
     <div className="h-screen">
       <div className="flex flex-col items-center justify-between h-full gap-y-5  ">
-        <div className="flex items-center justify-center">
-          <img
-            src="/icons/Background.svg"
-            className="absolute w-full h-full object cover"
-            alt="Background"
-          ></img>
-          <img
-            src="/logo.png"
-            className="absolute w-[50vw] h-[30vh] "
-            alt="Logo"
-          ></img>
+        <div className="flex justify-center">
+          <Image src="/icons/Background.svg" width={100} height={100} alt={"Logo"} className="absolute -z-10 w-full h-[100vh] object-cover" />
+          <Image src="/logo.png" width={200} height={200} alt={"Logo"} className="mt-15 absolute z-10 w-[70vw] object-contain" />
         </div>
         <div className="pb-20">
-          <Link href="/onboarding_3">
-            <button className="bg-[#69554D] hover:bg-[#29201D] text-white text-2xl  py-2 px-4 rounded w-60 h-15">
+          <Link href="/preferences">
+            <button className="bg-[#69554D] hover:bg-[#29201D] text-white text-2xl py-2 px-4 rounded w-60 h-15 mb-15">
               Yes!
             </button>
           </Link>
         </div>
+      </div>
       </div>
   );
 }
