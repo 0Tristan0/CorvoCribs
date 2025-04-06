480e3dab259817e8ab500b2028e0c54f795171ba
0Tristan0 <goehrint@oregonstate.edu>
2025-04-06 00:10:12 -0700

    fix error

--- corvo-cribs/app/onboarding_1/page.js
+++ corvo-cribs/app/onboarding_1/page.js
@@ -4,13 +4,13 @@ import Link from "next/link";
 export default function Onboarding_1() {
   return (
     <div className="h-screen">
-      <div className="pt-15 flex flex-col items-center justify-between h-full gap-y-5  ">
+      <div className="pt-5 flex flex-col items-center justify-between h-full gap-y-5  ">
         <h1 className="text-3xl font-semibold text-center m-4">
           Looking for housing your next academic term?
         </h1>
         <div className="pb-20 flex items-center justify-center">
-          <img src="/beavers/Beaver-Lodge.PNG" className="w-[80vw] absolute z-10" alt="Beaver Lodge"></img>
-          <img src="/icons/Trees_Lodge.svg" className="w-[100vw] absolute" alt= "Trees Lodge"></img>
+          <img src="/beavers/Beaver-Lodge.PNG" className=" max-w-[300px] w-[80vw] absolute z-10" alt="Beaver Lodge"></img>
+          <img src="/icons/Trees_Lodge.svg" className="max-w-[350px] w-[100vw] absolute" alt= "Trees Lodge"></img>
         </div>
         <div className="pb-15">
           <Link href="/onboarding_2">
