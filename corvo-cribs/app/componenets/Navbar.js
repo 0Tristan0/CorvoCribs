1b692a210cbba705013210692b1ff453c4f19cf5
Luca Denhez <lucadenhez@icloud.com>
2025-04-05 17:44:13 -0700

    Done with nav bar

--- corvo-cribs/app/componenets/Navbar.js
+++ corvo-cribs/app/componenets/Navbar.js
@@ -1,14 +1,16 @@
-import Image from "next/image";
 import PreferencesIcon from "./icons/PreferencesIcon";
+import HomeIcon from "./icons/HomeIcon";
+import FavoritesIcon from "./icons/FavoritesIcon";
+import MapIcon from "./icons/MapIcon";
 
 export default function Navbar({ currentIndex = 2 }) {
     return (
         <div>
             <nav className="flex items-center justify-between flex-wrap p-6">
-                <PreferencesIcon fill={(currentIndex ? 0 : "#D2D2D2")} className="h-10 w-auto" />
-                <Image src="/icons/map.svg" alt="Logo" width={100} height={50} className="h-10 w-auto" />
-                <Image src="/icons/home.svg" alt="Logo" width={100} height={50} className="h-10 w-auto" />
-                <Image src="/icons/favorites.svg" alt="Logo" width={100} height={50} className="h-10 w-auto" />
+                <PreferencesIcon fill={(currentIndex == 0 ? "#D2D2D2": "#F5E5AC")} className="h-10 w-auto" />
+                <MapIcon fill={(currentIndex == 1 ? "#FACBCB" : "#F5E5AC")} className="h-10 w-auto" />
+                <HomeIcon fill={(currentIndex == 2 ? "#69554D": "#F5E5AC")} className="h-10 w-auto" />
+                <FavoritesIcon fill={(currentIndex == 3 ? "#FFAC8B" : "#F5E5AC")} className="h-10 w-auto" />
             </nav>
         </div>
     );
